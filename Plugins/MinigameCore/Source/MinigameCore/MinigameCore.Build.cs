b9454d4de44a3b866bfa4b6aa6a907d8320cbe24
mob205 <mobslayer205@gmail.com>
2025-07-08 17:13:52 -0400

    Update MinigameCore plugin. Fix resetting

--- Plugins/MinigameCore/Source/MinigameCore/MinigameCore.Build.cs
+++ Plugins/MinigameCore/Source/MinigameCore/MinigameCore.Build.cs
@@ -8,6 +8,17 @@ public class MinigameCore : ModuleRules
 	{
 		bUsePrecompiled = true;
 		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			});
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
